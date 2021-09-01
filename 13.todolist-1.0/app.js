c959649a3db43f494709d97ffb80d2c71b40a621
shobhamrj <shobham.rajak@gmail.com>
2021-09-01 23:30:56 +0530

    initialize and did some prerequisite for the to do app

--- 13.todolist-1.0/app.js
+++ 13.todolist-1.0/app.js
@@ -3,9 +3,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function(req, res) {
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
+    res.render("list", {
+        kindOfDay: day
+    });
+});
 
-app.get("/", function() {
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
 
 });
 
