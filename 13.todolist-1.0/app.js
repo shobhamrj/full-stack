4d6cf6e56ae1a51b5361989368b32be7939baaa1
shobhamrj <shobham.rajak@gmail.com>
2021-09-02 22:19:46 +0530

    create date and day module

--- 13.todolist-1.0/app.js
+++ 13.todolist-1.0/app.js
@@ -1,22 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const date = require(__dirname +"/date.js");
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = ["Eat", "Sleep", "Code", "Repeat"];
-let workItems = [];
+const items = ["Eat", "Sleep", "Code", "Repeat"];
+const workItems = [];
 
 app.get("/", function(req, res) {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US", options);
+    let day = date.getDate();
     res.render("list", { listTitle: day, newListItems: items });
 });
 app.post("/", function(req, res) {
