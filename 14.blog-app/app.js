6a451bd10bd3c4fbda1b97548aa939f09d206613
shobhamrj <shobham.rajak@gmail.com>
2021-09-03 02:28:15 +0530

    create routes for each page

--- 14.blog-app/app.js
+++ 14.blog-app/app.js
@@ -17,6 +17,15 @@ app.get('/', (req, res)=>{
   res.render('home', {startingContent: homeStartingContent});
 });
 
+app.get('/about', (req, res)=>{
+  res.render('about', {aboutContent: aboutContent});
+});
+
+app.get('/contact', (req, res)=>{
+  res.render('contact', {contactContent: contactContent});
+});
+
+
 
 
 
