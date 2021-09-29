14b73d4f8a480a0024bf91d13e46abb386bcd772
shobhamrj <shobham.rajak@gmail.com>
2021-09-29 23:23:05 +0530

    change foreach loop in ejs and add insertmany and find

--- 17.toDoList-1.2/app.js
+++ 17.toDoList-1.2/app.js
@@ -2,24 +2,40 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
-
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true}); 
+const itemsSchema = {
+  name: String
+};
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+  name: "Eat"
+});
+const item2 = new Item({
+  name: "Sleep"
+});
+const item3 = new Item({
+  name: "Code"
+});
+const defaultItems = [item1, item2, item3];
+Item.insertMany(defaultItems, (err)=>{
+  if(err){
+    console.log(err);
+  }else {
+    console.log("Sucessfully saved default items");
+  }
+});
 app.get("/", function(req, res) {
-
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+  Item.find({}, (err, foundItems)=>{
+    console.log(foundItems);
+    res.render("list", {listTitle: "Today", newListItems: foundItems});
+  });
+  
 });
 
 app.post("/", function(req, res){
