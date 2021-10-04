6c5b28fadf9512b69f2f4a8d4c13e0aa24e65fb4
shobhamrj <shobham.rajak@gmail.com>
2021-10-04 23:51:16 +0530

    add route params and update, delete

--- 19.Wiki-API/app.js
+++ 19.Wiki-API/app.js
@@ -19,46 +19,74 @@ const articleSchema = {
     content: String
 };
 const Article = mongoose.model("Article", articleSchema);
- 
-app.get("/articles", (req, res)=>{
-  Article.find({}, (err, foundArticles)=>{
-      if(!err){
-          res.send(foundArticles);
-      }
-      else {
-          res.send(err);
-      }
-  });
-});
 
-app.post("/articles", (req, res)=>{
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
-    });
-    newArticle.save((err)=>{
-        if(!err){
-            res.send("Sucessfully added to DB");
-        }
-        else {
-            res.send(err);
-        }
-    });
-});
+// for all article
 
-app.delete("/articles", (req, res)=>{
-    Article.deleteMany((err)=>{
-        if(!err){
-            res.send("Sucessfully deleted");
-        }
-        else {
-            res.send(err);
-        }
+app.route("/articles")
+    .get((req, res)=>{
+        Article.find({}, (err, foundArticles)=>{
+            if(!err){
+                res.send(foundArticles);
+            }
+            else {
+                res.send(err);
+            }
+        });
+    })
+    .post(
+        (req, res)=>{
+            const newArticle = new Article({
+                title: req.body.title,
+                content: req.body.content
+            });
+            newArticle.save((err)=>{
+                if(!err){
+                    res.send("Sucessfully added to DB");
+                }
+                else {
+                    res.send(err);
+                }
+            });
+    })
+    .delete(
+        (req, res)=>{
+            Article.deleteMany((err)=>{
+                if(!err){
+                    res.send("Sucessfully deleted");
+                }
+                else {
+                    res.send(err);
+                }
+            });
     });
-});
 
-app.listen(8080, ()=> {
-  console.log("Server started on port 8080");
+// for all article
+
+app.route("/articles/:articleTitle")
+    .get((req, res)=>{
+        Article.findOne({title: req.params.articleTitle}, (err, foundArticle)=>{
+            if(foundArticle){
+                res.send(foundArticle);
+            }
+            else {
+                res.send("No articles matching with that title");
+            }
+        });
+    })
+    .put((req, res)=>{
+        Article.updateOne(
+            {title: req.params.articleTitle},
+            {title: req.body.title, content: req.body.content},
+            {overwrite: true},
+            (err) =>{
+               if(!err){
+                   res.send("Sucessfully Updated article!")
+               }
+            });
+    })
+
+app.listen(3000, ()=> {
+  console.log("Server started on port 3000");
 });
 
 
