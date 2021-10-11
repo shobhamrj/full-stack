dddc7c032dfbbeb2a79e44ffe4725b9e9ac342ad
Shobham Rajak <shobham.rajak@gmail.com>
2021-10-12 00:42:57 +0530

    add header footer and a simple note taking tile

--- 20.keeper/src/index.js
+++ 20.keeper/src/index.js
@@ -13,8 +13,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/App"
 
 ReactDOM.render(
-  <h1> hello world !</h1>,
+  <App />,
   document.getElementById("root")
 );
