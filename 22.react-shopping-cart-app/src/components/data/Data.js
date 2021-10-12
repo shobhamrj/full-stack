8ff500d38a95ae45391f78bf1733268ded81042e
Shobham Rajak <shobham.rajak@gmail.com>
2021-10-13 01:18:07 +0530

    refactor shopping-cart-app

---
+++ 22.react-shopping-cart-app/src/components/data/Data.js
@@ -0,0 +1,52 @@
+ const data= {
+   productItems: [
+  {
+    id: "1",
+    name: "Oneil Dodson chinos",
+    price: "1,938.16",
+    image: "./images/image_8.jpg"
+  },
+
+
+  {
+    id: "2",
+    name: "Young Bruce shorts",
+    price: "1,153.99",
+    image: "./images/image_9.jpg"
+  },
+
+  {
+    id: "3",
+    name: "Young ",
+    price: "1,541.16",
+    image: "./images/image_7.jpg"
+  },
+ 
+  {
+    id: "4",
+    name: " Bruce ",
+    price: "1,231.16",
+    image: "./images/image_4.jpg"
+  },
+ 
+  {
+    id: "5",
+    name: "shorts",
+    price: "1,114.12",
+    image: "./images/image_5.jpg"
+  },
+ 
+  {
+    id: "6",
+    name: "Bruceyy shirty shirt",
+    price: "1,789.11",
+    image: "./images/image_6.jpg"
+  },
+ 
+ 
+ 
+
+]
+ }
+  
+export default data;
