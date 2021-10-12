12246d71acf0716f57119f836ae3b7f0f341928c
Shobham Rajak <shobham.rajak@gmail.com>
2021-10-13 01:10:24 +0530

    refactor

--- 21.react-props-practice/src/components/App.jsx
+++ 21.react-props-practice/src/components/App.jsx
@@ -23,24 +23,6 @@ function App() {
         img="https://avatars.githubusercontent.com/u/32607243?v=4"
       />
       {contacts.map(createCard)}
-      {/* <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      /> */}
     </div>
   );
 }
