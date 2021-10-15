0b1bc073fab046a5f46116fd394370abaedb8d99
Shobham Rajak <shobham.rajak@gmail.com>
2021-10-16 01:06:57 +0530

    complete emojipedia-app

--- 23.react-emojipedia-app/src/emojipedia.js
+++ 23.react-emojipedia-app/src/emojipedia.js
@@ -1,23 +1,39 @@
 const emojipedia = [
   {
     id: 1,
-    emoji: "💪",
-    name: "Tense Biceps",
+    emoji: "😀",
+    name: "Grinning Face",
     meaning:
-      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+      "The happy smiling face is one of the most common emojis and universally applicable: you just want to say hello, express joy or excitement about something or brighten up a short text."
   },
   {
     id: 2,
-    emoji: "🙏",
-    name: "Person With Folded Hands",
+    emoji: "😅",
+    name: "Smiling Face With Open Mouth & Cold Sweat",
     meaning:
-      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+      "Sweaty smile. Relief that a tight situation went well or was coped with positively, e.g. an important appointment. A difficult event is imminent and you are already nervous."
   },
   {
     id: 3,
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
+    emoji: "😇",
+    name: "Smiling Face With Halo",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+      "Would like to express its (possibly pretended) innocence. Someone does good deeds, is very sweet, innocent and behaves in an exemplary way. The angelic face can also be used humorously for rather not so good deeds or behaviors"
+  },
+  {
+    id: 4,
+    emoji: "😍",
+    name: " Smiling Face With Heart-Eyes",
+    meaning:
+      "Happily beaming face with heart-shaped eyes. Insanely in love: with a person, a place or an object. Expresses the extent of love and affection as well as gratitude and is often part of romantic messages. Just does not know what to do with all the luck. "
+  },
+  {
+    id: 5,
+    emoji: "😜",
+    name: "Face With Stuck-Out Tongue & Winking Eye",
+    meaning:
+      "Smiley boldly sticks out the tongue, winking with one eye. Has made a joke or wants to flirt with the chat partner. Is carefree and does not mean it seriously. "
   }
 ];
+
+export default emojipedia;
