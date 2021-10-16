5170e59ed2c9578ffe0b70477ac291f029d8edca
Shobham Rajak <shobham.rajak@gmail.com>
2021-10-17 00:50:41 +0530

    complete react time app

--- 24.react-time-app(useState-Hook-exercise)/src/components/App.jsx
+++ 24.react-time-app(useState-Hook-exercise)/src/components/App.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  let time = new Date().toLocaleTimeString();
+  setInterval(updateTime, 1000)
+  const now = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(now) 
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
   return (
     <div className="container">
       <h1> {time} </h1>
-      <button onClick={getCurrentTime}> Get Time </button>
+      <button onClick={updateTime}> Get Time </button>
     </div>
   );
 }
