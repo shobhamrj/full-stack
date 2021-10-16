f528a6136eadcf0bd75c192d7a5b725707fe6493
Shobham Rajak <shobham.rajak@gmail.com>
2021-10-17 00:40:05 +0530

    start react time app

--- 24.react-time-app(useState-Hook-exercise)/src/components/App.jsx
+++ 24.react-time-app(useState-Hook-exercise)/src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1> {time} </h1>
+      <button onClick={getCurrentTime}> Get Time </button>
     </div>
   );
 }
