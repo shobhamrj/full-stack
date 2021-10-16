daa16bdef949478a432637a1cb169eb6629b0370
Shobham Rajak <shobham.rajak@gmail.com>
2021-10-16 22:29:06 +0530

    complete login app

--- 22.react-loginPage/src/components/App.jsx
+++ 22.react-loginPage/src/components/App.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import Login from "./Login";
 
-var isLoggedIn = false;
-
-function renderConditionally() {
-  if(isLoggedIn) {
-    return <h1>Hello</h1> ;
-  } else {
-    return <Login />
-  }
-}
+var isLoggedIn = true;
 
 function App() {
   return (
-    <div className="container">
-      {renderConditionally()}
+    <div className="container">{
+      isLoggedIn === true ? <h1>Hello</h1> : <Login />
+    }
     </div>
   );
 }
