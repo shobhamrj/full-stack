e4aa31a04f9ad1e79f95d78d5ac752554285088a
Shobham Rajak <shobham.rajak@gmail.com>
2021-10-18 18:52:04 +0530

    refactor shoping cart app

---
+++ react-shopping-cart-app/src/components/App.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import data from "./data/Data"
+
+function App() {
+    const {items} = data;
+    console.log(items);
+}
+
+export default App;
---
+++ react-shopping-cart-app/src/components/buttons/sizeButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function sizeButton() {
+    return
+}
+
+export default sizeButton;
