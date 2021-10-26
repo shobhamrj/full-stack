8c249e54b52b43564db33b50b3b5a147fdeeffe7
Shobham Rajak <shobham.rajak@gmail.com>
2021-10-27 00:28:20 +0530

    working on keeper app again

--- keeper/src/components/App.jsx
+++ keeper/src/components/App.jsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Note from './Note';
-import notes from '../notes';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return ( 
+  return (
     <div>
-        <Header />
-        {notes.map((noteItem) => (
-        <Note 
-            key={noteItem.key}
-            title={noteItem.title}
-            content={noteItem.content}
-        />
-        ))};
-        <Footer />
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
     </div>
-    );
-
+  );
 }
 
 export default App;
